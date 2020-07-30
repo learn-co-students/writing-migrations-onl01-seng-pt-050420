cccc38ca205e20d213160340cc06cb434d4c3fa0
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-07-30 17:12:41 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,12 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
     
     def change
-        create_table :artists do |t|
+        create_table :students do |t|
           t.string :name
-          t.string :genre
-          t.integer :age
-          t.string :hometown
         end
     end
-    
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-    
-    create_table "artists", force: :cascade do |t|
-        t.string "name"
-        t.string "genre"
-        t.integer "age"
-        t.string "hometown"
-      end
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "grade"
+    t.datetime "birthdate"
+  end
 
 end
